bde060a69431457ab831ee4daeda8bc4a457db4b
Ben Ranson <benr444@bellsouth.net>
2017-07-02 18:31:03 -0400

    Merged Branches
    Issue with NodeRequest resolved

--- Project Vault Main/src/mainPage.php
+++ Project Vault Main/src/mainPage.php
@@ -8,7 +8,7 @@
 		<h1 align="center">Project Vault Network View</h1>
 		<script>Document.getElementById("mainP").innerHTML = sendNodeRequest(0);</script>
 		<p id="mainP">
-
+			If you see this text, an error has occured
 		</p>
 	</body>	
 </html>
